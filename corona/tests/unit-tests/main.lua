a4f747f59ea7cde902ac177a1d605180b31d3c6d
Devendra <devendra@udgama.com>
2013-08-22 21:17:14 +0530

    adding more tests

--- corona/tests/unit-tests/main.lua
+++ corona/tests/unit-tests/main.lua
@@ -17,7 +17,7 @@ local pubnub_obj = pubnub.new({
 
 local channel = "corona-lua-test-" .. math.random() .. "-"
 
-local total = 7
+local total = 25
 local pass = 0
 local fail = 0
 
@@ -144,6 +144,170 @@ local tests = {
                         pubnub_obj_2:subscribe({
                             channel = ch,
                             callback = function(r) end,
+                        })
+                    end)
+                end
+
+            })
+        end
+    },
+    {
+        
+        func = 
+        function()
+            name = "History Test"
+            local msg = "hi"
+            local ch = channel .. "5"
+            pubnub_obj:publish({
+                channel = ch,
+                message = msg,
+                callback = function(r)
+                    test(r[1] == 1, name .. " : " .. "Message should get published")
+                    pubnub_obj:history({
+                        channel = ch,
+                        count = 1,
+                        callback = function(r)
+                            test(r[1][1] == msg, name .. " : " .. "Message published should be available in history")
+                            test(r[1][2] == nil, name .. " : " .. "History Message count")
+                        end
+                    })
+                end,
+                error = function(e)
+                end
+            })
+        end
+    },
+    {
+        
+        func = 
+        function()
+            name = "History Test with 2 Published messages"
+            local msg1 = 1
+            local msg2 = 2
+            local ch = channel .. "6"
+            pubnub_obj:publish({
+                channel = ch,
+                message = msg1,
+                callback = function(r)
+                    test(r[1] == 1, name .. " : " .. "Message should get published")
+                    pubnub_obj:publish({
+                        channel = ch,
+                        message = msg2,
+                        callback = function(r)
+                            test(r[1] == 1, name .. " : " .. "Message should get published")
+                            timer.performWithDelay(5000, function()
+                                pubnub_obj:history({
+                                    channel = ch,
+                                    count = 1,
+                                    callback = function(r)
+                                        test(r[1][1] == msg2, name .. " : " .. "Message published should be available in history 1")
+                                        test(r[1][2] == nil, name .. " : " .. "History Message count")
+                                    end
+                                })
+                                pubnub_obj:history({
+                                    channel = ch,
+                                    callback = function(r)
+                                        test(r[1][1] == msg1, name .. " : " .. "Message published should be available in history 2")
+                                        test(r[1][2] == msg2, name .. " : " .. "Message published should be available in history 3")
+                                        test(r[1][3] == nil, name .. " : " .. "Message Count 2")
+                                    end
+                                })
+                            end)
+                        end,
+                        error = function(e)
+                        end
+                    })
+                    
+                end,
+                error = function(e)
+                end
+            })
+        end
+    },
+        {
+        
+        func = 
+        function()
+            name = "History Test with 2 Published messages with reverse true"
+            local msg1 = 1
+            local msg2 = 2
+            local ch = channel .. "7"
+            pubnub_obj:publish({
+                channel = ch,
+                message = msg1,
+                callback = function(r)
+                    test(r[1] == 1, name .. " : " .. "Message should get published")
+                    pubnub_obj:publish({
+                        channel = ch,
+                        message = msg2,
+                        callback = function(r)
+                            test(r[1] == 1, name .. " : " .. "Message should get published")
+                            timer.performWithDelay(5000, function()
+                                pubnub_obj:history({
+                                    channel = ch,
+                                    count = 1,
+                                    reverse = true,
+                                    callback = function(r)
+                                        test(r[1][1] == msg1, name .. " : " .. "Message published should be available in history 1")
+                                        test(r[1][2] == nil, name .. " : " .. "History Message count")
+                                    end
+                                })
+                                pubnub_obj:history({
+                                    channel = ch,
+                                    reverse = true,
+                                    callback = function(r)
+                                        test(r[1][1] == msg1, name .. " : " .. "Message published should be available in history 2")
+                                        test(r[1][2] == msg2, name .. " : " .. "Message published should be available in history 3")
+                                        test(r[1][3] == nil, name .. " : " .. "Message Count 2")
+                                    end
+                                })
+                            end)
+                        end,
+                        error = function(e)
+                        end
+                    })
+                    
+                end,
+                error = function(e)
+                end
+            })
+        end
+    },
+    {
+        
+        func = 
+        function()
+            local name = "Here Now Test"
+            local ch = channel .. "8"
+            local msg = {1,2,3}
+            local count = 0
+            local pubnub_obj_2
+            pubnub_obj:subscribe({
+                channel = ch,
+                callback = function(r) end,
+                connect = function(c)
+                    pubnub_obj_2 = pubnub.new({
+                        publish_key   = "demo",
+                        subscribe_key = "demo",
+                        secret_key    = "demo",
+                        ssl           = false,
+                        origin        = "pubsub.pubnub.com"
+                    })
+                    timer.performWithDelay(5000, function()
+                        pubnub_obj_2:subscribe({
+                            channel = ch,
+                            connect = function(r)
+                                timer.performWithDelay(5000, function()
+                                    pubnub_obj:here_now({
+                                        channel = ch,
+                                        callback = function(r)
+                                            test(r.occupancy == 2, name .. " : " .. "Here Now occupancy test")
+                                        end
+                                    })
+                                end)
+                            end,
+                            callback = function() end
+                            
                         })
                     end)
                 end
