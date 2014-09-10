375ed68c2d085a714e13b07c38f6f3ccedcee89d
gcohen <geremy@pubnub.com>
2014-09-10 15:13:18 -0700

    Added presence and history features

--- corona/examples/pub-sub-demo/main.lua
+++ corona/examples/pub-sub-demo/main.lua
@@ -14,133 +14,133 @@
 
 -- Require the widget library
 require "pubnub"
+local json = require "json"
 
-local widget = require( "widget" )
+local widget = require("widget")
 
 local background = display.newImage("carbonfiber.jpg", true) -- flag overrides large image downscaling
 background.x = display.contentWidth / 2
 background.y = display.contentHeight / 2
 
-local roundedRect = display.newRoundedRect( 10, 50, 300, 40, 8 )
-roundedRect.anchorX, roundedRect.anchorY = 0.0, 0.0 	-- simulate TopLeft alignment
-roundedRect:setFillColor( 0/255, 0/255, 0/255, 170/255 )
+local roundedRect = display.newRoundedRect(10, 50, 300, 40, 8)
+roundedRect.anchorX, roundedRect.anchorY = 0.0, 0.0 -- simulate TopLeft alignment
+roundedRect:setFillColor(0 / 255, 0 / 255, 0 / 255, 170 / 255)
 
-local t = display.newText( "waiting for button event...", 0, 0, native.systemFont, 18 )
+local t = display.newText("Waiting for subscribed message receipt...", 0, 0, native.systemFont, 10)
 t.x, t.y = display.contentCenterX, 70
+
+local outputWindow = display.newText("Waiting for Presence / History Events", 0, 0, 300, 100, native.systemFont, 12)
+outputWindow.x, outputWindow.y = display.contentCenterX, 450
 -------------------------------------------------------------------------------
 -- Create 5 buttons, using different optional attributes
 -------------------------------------------------------------------------------
 
 multiplayer = pubnub.new({
-    publish_key   = "demo",
+    publish_key = "demo",
     subscribe_key = "demo",
-    secret_key    = nil,
-    ssl           = nil,
-    origin        = "pubsub.pubnub.com"
+    secret_key = nil,
+    ssl = nil,
+    origin = "pubsub.pubnub.com"
 })
 
+channel = "z"
+
 multiplayer:subscribe({
-    channel  = "z",
+    channel = channel,
     callback = function(message)
-        t.text = "I RECEIVED: " .. message.msgtext
+        if message.msgtext ~= nil then
+            t.text = "Received the published message: " .. message.msgtext
+        else
+            t.text = "Make sure message has a 'msgtext' key, eg {'msgtext':'myMessage'}"
+        end
     end,
     errorback = function()
         print("Oh no!!! Dropped 3G Conection!")
+    end,
+    presence = function(message)
+        outputWindow.text = ""
+        local presenceOutput = ""
+        for i, v in pairs(message) do
+            presenceOutput = (i .. ": " .. v .. "\n")
+            outputWindow.text = outputWindow.text .. presenceOutput
+            print(presenceOutput)
+        end
     end
 })
 
 -- These are the functions triggered by the buttons
 
-local button1Press = function( event )
-	t.text = "publish"
+local getHistory = function(event)
+    t.text = "Fetching history..."
+
+    multiplayer:history({
+        count = 5,
+        channel = channel,
+        callback = function(message)
+            outputWindow.text = ""
+            local presenceOutput = ""
+                presenceOutput = (json.encode(message) .. "\n")
+                outputWindow.text = outputWindow.text .. presenceOutput
+                print(presenceOutput)
+        end,
+        errorback = function()
+            print("Oh no!!! Dropped 3G Conection!")
+        end
+    })
+end
+
+local publishMessage = function(event)
+    t.text = "Publishing message..."
 
     multiplayer:publish({
-        channel = "z",
+        channel = channel,
         message = { msgtext = "sent from corona!" }
     })
-
 end
 
-local button1Release = function( event )
-	t.text = "Button 1 released"
-end
-
-
-local buttonHandler = function( event )
-	t.text = "id = " .. event.target.id .. ", phase = " .. event.phase
+local buttonHandler = function(event)
+    t.text = "id = " .. event.target.id .. ", phase = " .. event.phase
 end
 
 
 -- This button has individual press and release functions
 -- (The label font defaults to native.systemFontBold if no font is specified)
 
-local button1 = widget.newButton
-{
-	defaultFile = "buttonRed.png",
-	overFile = "buttonRedOver.png",
-	label = "PUBLISH",
-	emboss = true,
-	onPress = button1Press
+local publishButton = widget.newButton{
+    defaultFile = "buttonRed.png",
+    overFile = "buttonRedOver.png",
+    label = "PUBLISH",
+    emboss = true,
+    onPress = publishMessage
 }
 
-
--- These other four buttons share a single event handler function, identifying themselves by "id"
--- Note that if a general "onEvent" handler is assigned, it overrides the "onPress" and "onRelease" handling
-
--- Also, some label fonts may appear vertically offset in the Simulator, but not on device, due to
--- different device font rendering. The button object has an optional "offset" property for minor
--- vertical adjustment to the label position, if necessary (example: offset = -2)
-
-local button2 = widget.newButton
-{
-	id = "button2",
-	defaultFile = "buttonYellow.png",
-	overFile = "buttonYellowOver.png",
-	label = "Button 2 Label",
-	labelColor = 
-	{ 
-		default = { 51, 51, 51, 255 },
-	},
-	font = native.systemFont,
-	fontSize = 22,
-	emboss = true,
-	onEvent = buttonHandler,
+local historyButton = widget.newButton{
+    id = "historyButton",
+    defaultFile = "buttonYellow.png",
+    overFile = "buttonYellowOver.png",
+    label = "HISTORY",
+    labelColor =
+    {
+        default = { 51, 51, 51, 255 },
+    },
+    font = native.systemFont,
+    fontSize = 22,
+    emboss = true,
+    onEvent = getHistory,
 }
 
-local button3 = widget.newButton
-{
-	id = "button3",
-	defaultFile = "buttonGray.png",
-	overFile = "buttonBlue.png",
-	label = "Button 3 Label",
-	font = native.systemFont,
-	fontSize = 28,
-	emboss = true,
-	onEvent = buttonHandler,
+local button3 = widget.newButton{
+    id = "button3",
+    defaultFile = "buttonGray.png",
+    overFile = "buttonBlue.png",
+    label = "Button 3 Label",
+    font = native.systemFont,
+    fontSize = 28,
+    emboss = true,
+    onEvent = buttonHandler,
 }
 
-local buttonSmall = widget.newButton
-{
-	id = "smallBtn",
-	defaultFile = "buttonBlueSmall.png",
-	overFile = "buttonBlueSmallOver.png",
-	label = " I'm Small",
-	fontSize = 12,
-	emboss = true,
-	onEvent = buttonHandler,
-}
-
--- Of course, buttons don't always have labels
-local buttonArrow = widget.newButton
-{
-	id = "arrow",
-	defaultFile = "buttonArrow.png",
-	overFile = "buttonArrowOver.png",
-	onEvent = buttonHandler,
-}
-
-button1.x = 160; button1.y = 160
-button2.x = 160; button2.y = 240
+publishButton.x = 160; publishButton.y = 160
+historyButton.x = 160; historyButton.y = 240
 button3.x = 160; button3.y = 320
-buttonSmall.x = 85; buttonSmall.y = 400
-buttonArrow.x = 250; buttonArrow.y = 400
+
