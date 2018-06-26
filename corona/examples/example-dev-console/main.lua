5b410d3f7e1c188612e44241ab12168deb9908a6
Srdjan Veljkovic <sveljko@gvs.rs>
2018-06-26 21:56:44 +0300

    Real Corona UI developer console
    The old one didn't work with the latest Corona, as it relied on "standard I/O" which is problematic in Corona.
    
    It now has real UI - labels, input fields, buttons...

--- corona/examples/example-dev-console/main.lua
+++ corona/examples/example-dev-console/main.lua
@@ -6,6 +6,10 @@ require "crypto"
 require "PubnubUtil"
 
 
+local textout = PubnubUtil.textout
+local widget = require "widget"
+local fontSize = display.contentWidth/25
+
 -- INITIALIZE PUBNUB STATE
 --
 local pubnub_obj = pubnub.new({
@@ -16,63 +20,13 @@ local pubnub_obj = pubnub.new({
     origin        = "pubsub.pubnub.com"
 })
 
-local function notifyUser(x)
-	print(x)
-end
-
-local x = 0
-
-
-local function get_input(msg,valtype,default)
-	local x
-	local y
-	while type(x) ~= valtype do
-		print(msg .. " (" .. valtype .. ") :")
-		x = io.read()
-		y = x
-
-		if x == nil and default ~= nil then return default end
-
-		if valtype == "number" then
-			x = tonumber(x) or nil
-		end
-
-		if valtype == "boolean" then
-			x =  x == "true" or x == "True" or x == "yes" or x == "Yes" or x == "y" or x == "Y" or nil
-			if (x == nil) then
-				y = y == "false" or y == "False" or y == "no" or y == "No" or y == "n" or y == "N" or nil
-				if (y == true) then x = false else x = nil end
-			end
-		end
-	end
-
-	return x
-end
-
 
 local function print_to_console(x)
 	if type(x) == "table" then
-		print(table.tostring(x))
+		textout(table.tostring(x))
 	else
-		print(x)
+		textout(x)
 	end
-end
-
-local function subscribe_handler()
-	local channel = get_input("Enter Channel Name", "string")
-	local restore = get_input("Restore ?", "boolean")
-	local presence = get_input("Presence ?", "boolean")
-	local input = {
-		channel  = channel,
-		restore  = restore,
-		callback = function(x)  print_to_console(x) end,
-		connect  = function(x) print_to_console('CONNECTED to ' .. channel) end,
-		disconnect = function(x) print_to_console('DISCONNECTED from ' .. channel) end,
-		reconnect = function(x) print_to_console('RECONNECTED to ' .. channel) end,
-		error  	 = function(e)  print_to_console(e) end
-	}
-	if (presence) then input['presence'] = function(x) print_to_console(x) end end
-	pubnub_obj:subscribe(input)
 end
 
 local function current_sub_channel_list()
@@ -83,122 +37,214 @@ local function current_sub_channel_list()
 	return " ( Currently Subscribed to " .. list_str .. ")"
 end
 
+local function subscribe_handler(with_presence)
+	local chn = channel_name.text
+	local input = {
+		channel  = chn,
+		restore  = restore,
+		callback = function(x)  print_to_console(x) end,
+		connect  = function(x) print_to_console('CONNECTED to ' .. chn) end,
+		disconnect = function(x) print_to_console('DISCONNECTED from ' .. chn) end,
+		reconnect = function(x) print_to_console('RECONNECTED to ' .. chn) end,
+		error  	 = function(e)  print_to_console(e) end
+	}
+	if with_presence then input['presence'] = function(x) print_to_console(x) end end
+	pubnub_obj:subscribe(input)
+	textout(current_sub_channel_list())
+end
+
+local function subscribe_wout_presence_handler() subscribe_handler(false) end
+local function subscribe_with_presence_handler() subscribe_handler(true) end
+
 local function publish_handler()
-	local channel = get_input("Enter Channel Name", "string")
-	local message = get_input("Enter message", "string")
+	textout("publishing "..param.text.." on "..channel_name.text)
 	pubnub_obj:publish({
-		channel  = channel,
-		message  = message,
+		channel  = channel_name.text,
+		message  = param.text,
 		callback = function(r)  print_to_console(r) end,	
 		error  	 = function(e)  print_to_console(e) end
 	})
 end
 
-local function history_handler()
-	local channel = get_input("Enter Channel Name", "string")
-	local count = get_input("Enter Count", "number")
-	local reverse = get_input("Reverse?", "boolean")
+local function history_handler_ex(reversed)
+	local count = tonumber(param.text) or 10
+	--textout("Getting history in "..count.." records"..(reversed and ", reversed" or ""))
 	pubnub_obj:history({
-		channel = channel, 
+		channel = channel_name.text, 
 		count = count,
-		reverse = reverse,
-		callback = function(r)  print_to_console(r) end,	
+		reverse = reversed,
+		callback = function(r)
+			textout("Message history ("..#r[1].." messages):")
+			--for i = 1, #r[1] do
+			--	textout("    "..r[1][1])
+			--end
+			textout("    "..table.tostring(r[1]))
+		end,
 		error  	 = function(e)  print_to_console(e) end
 	})
 end
+
+local function history_handler() history_handler_ex(false) end
+local function history_reversed_handler() history_handler_ex(true) end
 
 local function time_handler()
-	pubnub_obj:time(function(t) print(t) end)
+	pubnub_obj:time(function(t) textout(string.format("%.f", t)) end)
 end
 
 local function here_now_handler()
-	local channel = get_input("Enter Channel Name", "string")
 	pubnub_obj:here_now({
-		channel = channel, 
+		channel = channel_name.text, 
 		callback = function(r)  print_to_console(r) end,	
 		error  	 = function(e)  print_to_console(e) end	
 	})
 end
 
-local function unsubscribe_handler()
-	pubnub_obj:unsubscribe({
-		channel = channel	
+local function init_handler()
+	local seckey = get_input("Enter Secret Key", "string")
+
+	pubnub_obj = pubnub.new({
+	    publish_key   = "demo",
+	    subscribe_key = "demo",
+	    secret_key    = seckey,
+	    ssl           = ssl,
+	    origin        = "pubsub.pubnub.com"
 	})
 end
 
-local function init_handler()
-	local pubkey = get_input("Enter Publish Key", "string", "demo")
-	local subkey = get_input("Enter Subscribe Key", "string", "demo")
-	local seckey = get_input("Enter Secret Key", "string")
-	local ssl 	 = get_input("SSL ?","boolean",false)
-	local origin = get_input("Enter origin", "string", "pubsub.pubnub.com")
-
+local function ssl_handler(newstate)
 	pubnub_obj = pubnub.new({
-	    publish_key   = pubkey,
-	    subscribe_key = subkey,
-	    secret_key    = seckey,
-	    ssl           = ssl,
-	    origin        = origin
+	    publish_key   = pubnub_obj.publish_key,
+	    subscribe_key = pubnub_obj.subscribe_key,
+	    secret_key    = pubnub_obj.secret_key,
+	    ssl           = newstate,
+	    origin        = pubnub_obj.origin
 	})
+end
+
+local function ssl_on_handler() 
+	ssl_handler(true) 
+	textout("SSL is now: On")
+end
+local function ssl_off_handler() 
+	ssl_handler(false) 
+	textout("SSL is now: Off")
+end
+
+local function secret_key_handler()
+	pubnub_obj = pubnub.new({
+	    publish_key   = pubnub_obj.publish_key,
+	    subscribe_key = pubnub_obj.subscribe_key,
+	    secret_key    = param.text,
+	    ssl           = pubnub_obj.ssl,
+	    origin        = pubnub_obj.origin
+	})
+	local seckey = pubnub_obj.secret_key
+	textout(" ( Current secret key : " .. ( seckey or "") .. ")")
 end
 
 local function auth_key_set_handler()
-	local auth = get_input("Enter auth key", "string")
-	pubnub_obj:set_auth_key(auth)
-end
-
-local function current_auth_key()
+	pubnub_obj:set_auth_key(param.text)
 	local authkey = pubnub_obj:get_auth_key()
-	return " ( Current auth key : " .. ( authkey or "") .. ")"
+	textout(" ( Current auth key : " .. ( authkey or "") .. ")")
 end
 
 local function unsubscribe_handler()
-	local channel = get_input("Enter Channel Name", "string")
-	local input = {
-		channel  = channel,
-	}
-	pubnub_obj:unsubscribe(input)
+	textout("unsubscribe from: "..channel_name.text)
+	pubnub_obj:unsubscribe{channel = channel_name.text }
+	textout(current_sub_channel_list())
 end
 
-local cmd_table = {
-	{cmd = "Subscribe", handler = subscribe_handler, extra = current_sub_channel_list},
-	{cmd = "Publish", handler = publish_handler},
-	{cmd = "History", handler = history_handler},
-	{cmd = "Here Now", handler = here_now_handler},
-	{cmd = "Unsubscribe", handler = unsubscribe_handler},
-	{cmd = "Time", handler = time_handler},
-	{cmd = "Init", handler = init_handler},
-	{cmd = "Auth Key Setting", handler = auth_key_set_handler, extra = current_auth_key}
-}
-	
-local function last_cmd()
-	local x = 1
-	for k,v in next,cmd_table do
-    	x = x + 1
+
+local function textWidth(the_text)
+	local tempTxt = display.newText(the_text, 0, 0, nil, fontSize)
+	local rslt = tempTxt.width
+	tempTxt:removeSelf()
+	return rslt
+end
+
+local function textHeight(the_text)
+	local tempTxt = display.newText(the_text, 0, 0, nil, fontSize)
+	local rslt = tempTxt.height
+	tempTxt:removeSelf()
+	return rslt
+end
+
+local spacing = textHeight("Ty") * 0.25
+local button_y_pos = spacing
+
+-- 
+-- CREATE developer channel and param TEXT INPUT FIELDs
+-- 
+local channel_text = display.newText("Channel", spacing, button_y_pos, nil, fontSize)
+channel_text.anchorX = 0
+channel_text.anchorY = 0
+button_y_pos = button_y_pos + channel_text.height + spacing
+channel_name = native.newTextField(spacing, button_y_pos, display.contentWidth - 2*spacing, 6*spacing)
+channel_name.text = "hello_world"
+channel_name.anchorX = 0
+channel_name.anchorY = 0
+button_y_pos = button_y_pos + channel_name.height + spacing
+ 
+local param_text = display.newText("Parameter", spacing, button_y_pos, nil, fontSize)
+param_text.anchorX = 0
+param_text.anchorY = 0
+button_y_pos = button_y_pos + param_text.height + spacing
+param = native.newTextField(spacing, button_y_pos, display.contentWidth - 2*spacing, 6*spacing)
+param.placeholder = "(publish message, history count, auth/secret key)"
+param.anchorX = 0
+param.anchorY = 0
+button_y_pos = button_y_pos + param.height + spacing
+
+--
+-- Buttons
+--
+
+button_y_pos = button_y_pos + spacing
+local button_x_pos = 10
+
+local function add_button(label, f)
+	local btn = widget.newButton{
+		id = "zec",
+		label = label,
+		left = button_x_pos,
+		top = button_y_pos,
+		shape = "roundedRect",
+		width = textWidth(label) + 2*spacing,
+		emboss = true,
+		cornerRadius = 8,
+		strokeWidth = 4,
+		fontSize = fontSize,
+		labelColor = {default={0,0,1,0.8}, over={0,0,0, 0.5}},
+		onEvent = function (e)
+			if e.phase == "ended" then
+				f()
+			end
+		end
+		}
+	--btn.anchorY = 1
+	button_x_pos = button_x_pos + btn.width + spacing
+	if button_x_pos + btn.width > display.contentWidth then 
+		button_x_pos = spacing
+		button_y_pos = button_y_pos + btn.height + 2*spacing
 	end
-	return x 
 end
 
-local function print_menu()
-	local x = 1
-	for k,v in next,cmd_table do
-		local str = "Enter " .. k .. " for " .. v.cmd
-		if (v.extra ~= nil) then str = str .. v.extra() end
-    	print(str)
-    	x = x + 1
-	end
-	print("Enter " .. x .. " to exit")
-	print("Enter " .. x + 1 .. " or more to specify interval in seconds\nafter which command input should be asked")
-end
+add_button("Subscribe", subscribe_wout_presence_handler)
+add_button("Unubscribe", unsubscribe_handler)
+add_button("Subscribe w/presence", subscribe_with_presence_handler)
+add_button("Publish", publish_handler)
+add_button("History", history_handler)
+add_button("History, reversed", history_reversed_handler)
+add_button("Time", time_handler)
+add_button("Here Now", here_now_handler)
+add_button("Set Auth Key", auth_key_set_handler)
+add_button("Set Secret Key", secret_key_handler)
+add_button("SSL On", ssl_on_handler)
+add_button("SSL Off", ssl_off_handler)
 
+--
+-- Position the "terminal" below buttons
+--
 
-local function get_command()	
-	print_menu()
-	x = get_input("Please enter a command", "number")
-	if (cmd_table[x]) then cmd_table[x].handler() end
-	local delay = 10
-	if x > last_cmd() then delay = x end
-	if (x ~= last_cmd()) then timer.performWithDelay( delay * 1000, get_command) end
-end
-
-timer.performWithDelay( 1 * 1000, get_command)
+if button_x_pos > spacing then button_y_pos = button_y_pos + spacing * 8 end
+PubnubUtil.set_starty(button_y_pos + spacing)
